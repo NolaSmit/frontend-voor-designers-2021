81e40c4e2d43d0c55a8b210c8e433b15ae73c27f
NolaSmit <74206066+NolaSmit@users.noreply.github.com>
2021-02-09 15:12:48 +0100

    Add files via upload

--- opdracht1/demo/js/script.js
+++ opdracht1/demo/js/script.js
@@ -4,3 +4,13 @@
   2. Koppel een evenListener aan het element om een mouse-event te detecteren
   3. Gebruik het Classlist object om een css class aan een element toe te voegen of weg te halen.
 */
+
+var hearts = document.querySelector("#heart");
+
+var icon = document.querySelector('i.far fa-heart');
+
+function addToFavourites() {
+    icon.classList.toggle('.fas fa-heart');
+}
+
+hearts.addEventListener('click', addToFavourites);
