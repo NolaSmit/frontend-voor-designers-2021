f86b6bc72eaf50c295ec8e10e158c9ef8705f4c2
NolaSmit <74206066+NolaSmit@users.noreply.github.com>
2021-02-10 13:56:44 +0100

    Add files via upload

--- opdracht1/demo/js/script.js
+++ opdracht1/demo/js/script.js
@@ -1,11 +1,17 @@
-/*var hearts = document.querySelector("#heart");
-console.log(hearts);*/
-
-var icon = document.querySelector('.far');
+/*Alle elementen met class .far selecteren*/
+var icon = document.querySelectorAll('.far');
 console.log (icon);
 
-function addToFavourites() {
-    icon.classList.toggle('fas');
+/*Uit code van front-end blok 2*/
+/* elke button laten luisteren naar een klik */
+/* na een klik de functie 'toggleLove' uitvoeren */
+for (i = 0; i < icon.length; i++) {
+  icon[i].addEventListener("click", addToFavourites);
 }
 
-icon.addEventListener('click', addToFavourites);
+function addToFavourites(event) {
+    /* het hartje waarop geklikt is in de variabele 'clickedHeart' opslaan */
+  var clickedHeart = event.target;
+    
+    clickedHeart.classList.toggle('fas');
+}
